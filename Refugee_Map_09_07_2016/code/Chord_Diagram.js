52d55deea4a64a90a019e9639d9214c0388b8834
VikingProgrammer <benobe22@gmail.com>
2016-08-09 12:34:15 +0100

    Updated the CSV file with new column, percentage change

--- Refugee_Map_09_07_2016/code/Chord_Diagram.js
+++ Refugee_Map_09_07_2016/code/Chord_Diagram.js
@@ -1,4 +1,6 @@
-var outerRadius = 960 / 2,
+var unique_countries = ['ABW', 'AFG', 'ALB', 'ALG', 'ANG', 'ANT', 'ARE', 'ARG', 'ARM', 'AUL', 'AUS', 'AZE', 'BAH', 'BAR', 'BDI', 'BEL', 'BEN', 'BGD', 'BHS', 'BHU', 'BKF', 'BLR', 'BOL', 'BOT', 'BRA', 'BSN', 'BUL', 'BZE', 'CAM', 'CAN', 'CAR', 'CAY', 'CHD', 'CHI', 'CHL', 'CMR', 'COB', 'COD', 'COI', 'COL', 'COS', 'CUB', 'CUW', 'CVI', 'CYP', 'CZE', 'DEN', 'DJB', 'DMA', 'DOM', 'ECU', 'EGU', 'ERT', 'EST', 'ETH', 'FIJ', 'FIN', 'FRA', 'GAB', 'GAM', 'GAZ', 'GBR', 'GEO', 'GFR', 'GHA', 'GNB', 'GRE', 'GRN', 'GUA', 'GUI', 'GUY', 'HAI', 'HKG', 'HON', 'HRV', 'HUN', 'ICE', 'ICO', 'IND', 'INS', 'IRE', 'IRN', 'IRQ', 'ISR', 'ITA', 'JAM', 'JOR', 'JPN', 'KAZ', 'KEN', 'KGZ', 'KOR', 'KOS', 'KRN', 'KUW', 'LAO', 'LBR', 'LBY', 'LCA', 'LEB', 'LES', 'LIE', 'LKA', 'LTU', 'LUX', 'LVA', 'MAD', 'MAU', 'MCD', 'MCO', 'MDA', 'MDV', 'MEX', 'MLI', 'MLS', 'MLW', 'MNE', 'MNG', 'MOR', 'MOZ', 'MTA', 'MTS', 'MYA', 'NAM', 'NEP', 'NET', 'NGR', 'NIC', 'NIG', 'NIU', 'NOR', 'NRU', 'NZL', 'OMN', 'PAK', 'PAN', 'PAR', 'PER', 'PHI', 'PLW', 'PNG', 'POL', 'POR', 'QAT', 'ROM', 'RSA', 'RUS', 'RWA', 'SAL', 'SAU', 'SEN', 'SEY', 'SIN', 'SLE', 'SOL', 'SOM', 'SPA', 'SRB', 'SRV', 'SSD', 'STA', 'STK', 'STP', 'SUD', 'SUR', 'SVK', 'SVN', 'SWA', 'SWE', 'SWI', 'SYR', 'TAN', 'TCI', 'THA', 'TIB', 'TJK', 'TKM', 'TMP', 'TOG', 'TON', 'TRT', 'TUN', 'TUR', 'UAE', 'UGA', 'UKR', 'URU', 'USA', 'UZB', 'VAR', 'VCT', 'VEN', 'WSH', 'YEM', 'ZAM', 'ZIM']
+var test_countries = ['BEL','ICO','SYR','GBR','GHA']
+var outerRadius = (screen.height*0.9) / 2,
     innerRadius = outerRadius - 130;
 
 var fill = d3.scale.category20c();
@@ -18,40 +20,42 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
 
-d3.json("readme.json", function(error, imports) {
+var datatset = []
+
+d3.csv("https://raw.githubusercontent.com/VikingProgrammer/VikingProgrammer.github.io/master/Refugee_Map_09_07_2016/data/clean_refugee_2015.csv", function(error,data) {
   if (error) throw error;
+  console.log(data)
+  dataset = data.map(function(d){return [d["country of asylum"],d["country of origin"],+d["total movement"]];})
+  var matrix = [],
+      new_matrix = []
 
-  var indexByName = d3.map(),
-      nameByIndex = d3.map(),
-      matrix = [],
-      n = 0;
-
-  // Returns the Flare package name for the given class name.
-  function name(name) {
-    return name.substring(0, name.lastIndexOf(".")).substring(6);
+  var i,j
+  for(i=0;i<196;i++){
+    matrix[unique_countries[i]] = []
+    for(j=0;j<196;j++){
+      matrix[unique_countries[i]][unique_countries[j]] = 0
+    }
+  }
+  console.log(dataset)
+  dataset.forEach(function(d){
+    if ((unique_countries.indexOf(d[0])>=0 && unique_countries.indexOf(d[1])>=0)){
+      if (d[2] < 0){
+        matrix[d[0]][d[1]] = 0
+      } else {
+        matrix[d[0]][d[1]] = d[2]
+      }
+    }
+  });
+for(i=0;i<196;i++){
+    new_matrix[i] = new Array(196);
+    for(j=0;j<196;j++){
+      new_matrix[i][j] = matrix[unique_countries[i]][unique_countries[j]] 
+    }
   }
 
-  // Compute a unique index for each package name.
-  imports.forEach(function(d) {
-    if (!indexByName.has(d = name(d.name))) {
-      nameByIndex.set(n, d);
-      indexByName.set(d, n++);
-    }
-  });
-
-  // Construct a square matrix counting package imports.
-  imports.forEach(function(d) {
-    var source = indexByName.get(name(d.name)),
-        row = matrix[source];
-    if (!row) {
-     row = matrix[source] = [];
-     for (var i = -1; ++i < n;) row[i] = 0;
-    }
-    d.imports.forEach(function(d) { row[indexByName.get(name(d))]++; });
-  });
-
-  chord.matrix(matrix);
-
+  console.log(new_matrix)
+  chord.matrix(new_matrix);
+  console.log(chord);
   var g = svg.selectAll(".group")
       .data(chord.groups)
     .enter().append("g")
@@ -71,7 +75,7 @@ d3.json("readme.json", function(error, imports) {
             + (d.angle > Math.PI ? "rotate(180)" : "");
       })
       .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-      .text(function(d) { return nameByIndex.get(d.index); });
+      .text(function(d) { return d[2]; });
 
   svg.selectAll(".chord")
       .data(chord.chords)
