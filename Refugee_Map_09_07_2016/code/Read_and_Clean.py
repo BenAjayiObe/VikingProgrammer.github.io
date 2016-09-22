0d160ccf4446222db758312173739ea9d97a5768
VikingProgrammer <benobe22@gmail.com>
2016-09-22 18:45:12 +0100

    Updated Looks

--- Refugee_Map_09_07_2016/code/Read_and_Clean.py
+++ Refugee_Map_09_07_2016/code/Read_and_Clean.py
@@ -2,7 +2,9 @@ import pandas as pd
 from pandas import DataFrame
 import numpy as np
 import math
+import scipy as sp
 from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
 # Data Source: https://data.humdata.org/dataset/2015-unhcr-refugees-statistics-by-coo-coa/resource/278278d5-ba6c-45eb-b34d-c0b8cbdbe223
 # #year,#adm1+code,#adm1+code,#affected+code,#affected,#affected
 if(0):
@@ -66,13 +68,19 @@ if(0):
 data = DataFrame.from_csv("../data/cleaned_name_corrected_refugee_2015.csv",sep=",",index_col=False,header=0)
 
 data = data.loc[data['Total_Movement']>0]
+
+data["Summed_Movement"] = data.groupby(["Country_Of_Origin","Country_Of_Asylum"])["Total_Movement"].transform("sum")
+data = data.drop_duplicates(subset=["Country_Of_Origin","Country_Of_Asylum"])
+
+print min(data['Summed_Movement'])
+print max(data['Summed_Movement'])
 Origin = data.Country_Of_Origin.unique().tolist()
 Asylum = data.Country_Of_Asylum.unique().tolist()
 
 Origin_Indexed = range(len(Origin))
 Asylum_Indexed = range(len(Asylum))
 
-w = 7
+w = 8
 h = len(data.index)
 Matrix = [[0 for x in range(w)] for y in range(h)]
 
@@ -83,7 +91,29 @@ for index, row in data.iterrows():
 	data.set_value(index,"Country_Of_Origin_Indexed",Origin.index(row["Country_Of_Origin"]))
 	data.set_value(index,"Country_Of_Asylum_Indexed",Asylum.index(row["Country_Of_Asylum"]))
 
-linescale = interp1d([1,945650],[0.1,50])
+def log_interp1d(xx, yy, kind='linear'):
+    logx = np.log10(xx)
+    logy = np.log10(yy)
+    lin_interp = sp.interpolate.interp1d(logx, logy, kind=kind)
+    log_interp = lambda zz: np.power(10.0, lin_interp(np.log10(zz)))
+    return log_interp
+
+def log_interp(zz, xx, yy):
+    logz = np.log10(zz)
+    logx = np.log10(xx)
+    logy = np.log10(yy)
+    return np.power(10, np.interp(logz, logx, logy))
+
+def linescale(df):
+    return (np.sqrt(df)/30)+1
+
+
+theDomain = np.arange(1,945655,1)
+theRange = np.arange(3,30,(27.0/945654.0))
+print(len(theDomain))
+print(len(theRange))
+#linescale = log_interp1d(theDomain,theRange,"linear")
+linescale2 = log_interp(50,theDomain,theRange)
 Origin_dict = {}
 Aslyum_dict = {}
 Origin_lines_pos = [0 for x in range(len(Origin))]
@@ -92,7 +122,7 @@ i = 0
 for row in Matrix:
 	row[0] = data.iloc[i]["Country_Of_Origin_Indexed"]	# The number denoting the country of Origin.
 	row[1] = data.iloc[i]["Country_Of_Asylum_Indexed"]	# The number denoting the country of Asylum.
-	row[2] = data.iloc[i]["Total_Movement"] 			# The number denoting the total movement.
+	row[2] = data.iloc[i]["Summed_Movement"] 			# The number denoting the total movement.
 	Origin_lines_pos[row[0]] += linescale(row[2])
 	Asylum_lines_pos[row[1]] += linescale(row[2])
 	row[3] = Origin_lines_pos[row[0]]					# The cumulated thickness of all the lines before it for that country.
@@ -120,22 +150,23 @@ for x in Asylum_lines_pos:
 for row in Matrix:
 	row[5] = Origin_lines_cumul[row[0]]					# The cumulated thickness of all the previous origin countries.
 	row[6] = Asylum_lines_cumul[row[1]]					# The cumulated thickness of all the previous asylum countries.
+	row[7] = linescale(row[2])
 
-# print "Origin Names\n\n"
-# print Origin
-# print "\n\n"
+print "Origin Names\n\n"
+print Origin
+print "\n\n"
 
-# print "Asylum Names\n\n"
-# print Asylum
-# print "\n\n"
+print "Asylum Names\n\n"
+print Asylum
+print "\n\n"
 
-# print "Origin_Indexed\n\n"
-# print Origin_Indexed
-# print "\n\n"
+print "Origin_Indexed\n\n"
+print Origin_Indexed
+print "\n\n"
 
-# print "Aslyum_Indexed\n\n"
-# print Asylum_Indexed
-# print "\n\n"
+print "Aslyum_Indexed\n\n"
+print Asylum_Indexed
+print "\n\n"
 
 print "Matrix\n\n"
 print Matrix
@@ -150,3 +181,25 @@ print "\n\n\n\n"
 
 print Origin_lines_cumul
 print Asylum_lines_cumul
+
+print len(data.index)
+
+
+
+#print(linescale(50))
+#print(log_interp(50,theDomain,theRange))
+#print(linescale(22032))
+#print(log_interp(22032,theDomain,theRange))
+
+# print(log_func(50,theDomain,theRange))
+# print(log_func(22032,theDomain,theRange))
+
+# df = np.arange(1,945655,1)
+# # print(log_func(df,theDomain,theRange))
+# plt.plot(df,log_interp(df,theDomain,theRange))
+# plt.plot(df,(np.log2(df)*1.6)/np.log(2))
+# plt.plot(df,(np.sqrt(df)/30)+3)
+# plt.show()
+
+data = {"data":Matrix, "position":[Origin_Indexed,Asylum_Indexed],"label":[Origin,Asylum],"label_width":[Origin_lines_pos,Asylum_lines_pos],"label_position":[Origin_lines_cumul,Asylum_lines_cumul]}
+print(data)
